5e65d012fe7fff3dd60b275a3f215333ba00f410
raivarunesh <rai.varunesh@gmail.com>
2021-09-14 12:20:22 +1000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+"""Change the config values."""
+branches = ("master",)  # Note: Single element requires a comma at end.
+add_codeowners_file = True
+signed_commit = False
+branch_rules = {"required_approving_review_count": 1,
+                "require_code_owner_reviews": True,
+                "contexts": ["CodeQL"],
+                "strict": True
+                }
