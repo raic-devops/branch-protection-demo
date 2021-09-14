f728adbd76b51ccef44b17c97564e41ec5e70f73
raivarunesh <rai.varunesh@gmail.com>
2021-09-14 14:05:37 +1000

    Update automate-branch-rules.py

--- automate-branch-rules.py
+++ automate-branch-rules.py
@@ -31,28 +31,28 @@ import codeowners
 #                       + repo.name + "," + branch_name)
 
 
-def add_one(pat):
-    """Add one function."""
-    #repo_name = input("\nRepository: ")
-    repo_name = "terraform-aws-secret-manager-with-rotation"
-    repo = git.get_repo(org_name+"/"+repo_name)
-    for branch_name in branches:
-        try:
-            repo.get_branch(branch_name)
-        except GithubException:
-            print("Error:", repo.name, ",", branch_name, "-->",
-                  sys.exc_info()[1])
-        else:
-            branch = repo.get_branch(branch_name)
-            if(add_codeowners_file):
-                codeowners.add(org_name, pat, repo_name, branch_name)
-            branch.edit_protection(**branch_rules)
-            if(signed_commit):
-                branch.add_required_signatures()
-            else:
-                branch.remove_required_signatures()
-            print("Edited the branch protection rules for: "
-                  + repo.name + "," + branch_name)
+# def add_one(pat):
+#     """Add one function."""
+#     #repo_name = input("\nRepository: ")
+#     repo_name = "terraform-aws-secret-manager-with-rotation"
+#     repo = git.get_repo(org_name+"/"+repo_name)
+#     for branch_name in branches:
+#         try:
+#             repo.get_branch(branch_name)
+#         except GithubException:
+#             print("Error:", repo.name, ",", branch_name, "-->",
+#                   sys.exc_info()[1])
+#         else:
+#             branch = repo.get_branch(branch_name)
+#             if(add_codeowners_file):
+#                 codeowners.add(org_name, pat, repo_name, branch_name)
+#             branch.edit_protection(**branch_rules)
+#             if(signed_commit):
+#                 branch.add_required_signatures()
+#             else:
+#                 branch.remove_required_signatures()
+#             print("Edited the branch protection rules for: "
+#                   + repo.name + "," + branch_name)
 
 
 # def remove_one():
@@ -76,31 +76,31 @@ def add_one(pat):
 #                       + repo.name + "," + branch.name)
 
 
-# def remove_all():
-#     """Remove all function."""
-#     print("")
-#     for repo in git.get_organization(org_name).get_repos():
-#         for branch_name in branches:
-#             try:
-#                 repo.get_branch(branch_name)
-#             except GithubException:
-#                 print("Error:", repo.name, ",",
-#                       branch_name, "-->", sys.exc_info()[1])
-#             else:
-#                 branch = repo.get_branch(branch_name)
-#                 if (branch.protected):
-#                     branch.remove_protection()
-#                     print("Removed branch protection rules for: "
-#                           + repo.name + "," + branch_name)
-#                 else:
-#                     print("No branch protection rules for: "
-#                           + repo.name + "," + branch.name)
+def remove_all():
+    """Remove all function."""
+    print("")
+    for repo in git.get_organization(org_name).get_repos():
+        for branch_name in branches:
+            try:
+                repo.get_branch(branch_name)
+            except GithubException:
+                print("Error:", repo.name, ",",
+                      branch_name, "-->", sys.exc_info()[1])
+            else:
+                branch = repo.get_branch(branch_name)
+                if (branch.protected):
+                    branch.remove_protection()
+                    print("Removed branch protection rules for: "
+                          + repo.name + "," + branch_name)
+                else:
+                    print("No branch protection rules for: "
+                          + repo.name + "," + branch.name)
 
 
 # header = open("header.txt", "r")
 # print(header.read())
 # org_name = input("\nGitHub Organization name: ")
-exec_type = 'O'
+exec_type = 'R'
 org_name = "raiv-devops"
 #pat = getpass("PAT: ")
 pat = "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"
@@ -114,11 +114,11 @@ git = Github(pat)
 
 if (exec_type == 'O'):
     add_one(pat)
-# if (exec_type == 'A'):
-#     add_all(pat)
-# elif (exec_type == 'S'):
-#     remove_one()
-# elif (exec_type == 'R'):
-#     remove_all()
+if (exec_type == 'A'):
+    add_all(pat)
+elif (exec_type == 'S'):
+    remove_one()
+elif (exec_type == 'R'):
+    remove_all()
 # else:
 #     print("Invalid input. Re-run")
